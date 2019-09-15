023b81a7faecb795b2dbf99d990f817323fa417d
Weber Calixto <webersouzacalixto@gmail.com>
2019-09-15 18:10:17 -0400

    Sering a file named car.jpg on /tmp

--- examples/showImg/showImgServer.cpp
+++ examples/showImg/showImgServer.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 #include <pistache/http.h>
 #include <pistache/router.h>
@@ -48,44 +49,33 @@ void showImgServer::setRoute()
 void showImgServer::jpegHandler(const Rest::Request &req, Http::ResponseWriter writer)
 {
 	auto        filename = req.param(":filename").as<std::string>();
-    std::ifstream file(filename, std::ios::binary | std::ios::ate);
-    std::streamsize size = file.tellg();
+	filename = "/tmp/car.jpg";
+	this->logger->log("showImgServer::jpegHandler START filename = "+filename);
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
+    std::streamsize fileSize = file.tellg();
+    this->logger->log("filesize = " + std::to_string(fileSize));
     file.seekg(0, std::ios::beg);
-    std::vector<char> buffer(size);
-    if (file.read(buffer.data(), size))
+    std::vector<char> buffer(fileSize);
+    if (file.read(buffer.data(), fileSize))
     {
         writer.setMime(MIME(Image, Jpeg));
-        auto os = writer.stream(Http::Code::Ok);
+        std::streamsize sz = 0;
+		std::streamsize sent = 0;
+		auto os = writer.stream(Http::Code::Ok);
+		try {
+		  while (sz < fileSize)
+		  {
+			if ((sent = os.write(reinterpret_cast<char*>(&buffer[sz]), (fileSize - sz) < 512L ? fileSize : 512L)) == 0) break;
+			sz += sent;
+		  }
+		}
+		catch (...)
+		{
+			this->logger->log("FAILURE SENDING DATA");
+		}
         os.ends();
+        return;
     }
-    else{
-    	auto os = writer.stream(Http::Code::Bad_Request);
-    	os.ends();
-    }
-
-
-   /* auto data = (drvFrameType == DRV_FRAME_JPEG) ? fotoFrame->cropRegionJPEG[idPista] : fotoFrame->cropRegionRGB[idPista];
-    const std::streamsize szData = (drvFrameType == DRV_FRAME_JPEG) ? fotoFrame->info.currentCropRegionJPEGSize[idPista] : fotoFrame->info.cropRegionMaxSize;
-    std::streamsize sz = 0;
-    std::streamsize sent = 0;
-    auto os = writer.stream(Http::Code::Ok);
-    try {
-      while (sz < szData)
-      {
-        if ((sent = os.write(reinterpret_cast<char*>(&data[sz]), std::min(512L, szData - sz))) == 0) break;
-        sz += sent;
-      }
-    }
-    catch (...)
-    {
-      NTCLOG("cam_get_picture_lane_by_trigger: catch de exception durante tranmissão");
-    }
-    os.ends();
-    return;
-  }
-  writer.send(Http::Code::Internal_Server_Error);*/
-
-
-
+    writer.send(Http::Code::Internal_Server_Error);
 }
 
